27c52a87665a6bc0cf0666832c9677fc7fc2cc22
Venkatesh-024 <kuppalavenkatesh9949@gmail.com>
2025-07-17 19:13:39 +0530

    add

--- src/main/java/com/tictactoe/config/WebConfig.java
+++ src/main/java/com/tictactoe/config/WebConfig.java
@@ -7,6 +7,6 @@ import org.springframework.web.servlet.config.annotation.*;
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("http://localhost:5175");
+        registry.addMapping("/**").allowedOrigins("http://localhost:5173");
     }
 }
